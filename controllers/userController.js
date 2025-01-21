11dcb69342647211266c2bee8c02412d5fd0f332
ikwame <iaagyapong@st.knust.edu.gh>
2025-01-21 23:52:05 +0000

    jwt auth

---
+++ controllers/userController.js
@@ -0,0 +1,79 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+//@desc Register a User
+//@route POST /api/user/register
+//access public
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const availableUser = await User.findOne({ email });
+  if (availableUser) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  //Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("The hashed password is ", hashedPassword);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  console.log(`user created is ${user}`);
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User data is not valid");
+  }
+});
+
+//@desc Login  User
+//@route POST /api/user/login
+//access public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const user = await User.findOne({ email });
+  //compare password with hashpassword
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {expiresIn: "1m"}
+    );
+    res.status(200).json({ accessToken });
+  }
+  else {
+    res.status(401);
+    throw new Error("Email or password is not valid");
+  }
+});
+
+
+//@desc Current User info
+//@route POST /api/user/current
+//access private
+const currentUser = asyncHandler(async (req, res) => {
+  res.json({ message: "Login user" });
+});
+
+module.exports = { registerUser, loginUser, currentUser };
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ connectDB();
 
 app.use(express.json());
 app.use('/api/contacts', require('./routes/contactRoutes'));
+app.use('/api/users', require('./routes/userRouter'));
 
 app.use(errorHandler);
 app.listen(PORT, () => {
@@ -21,3 +22,14 @@ app.listen(PORT, () => {
 // app.get("/api/contacts", (req, res) => {
 //   res.json({message:"Get all mf contacts"});
 // });
+
+//JWT web token
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.   header algorithm of the token
+// eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.  payload that has the user information
+// SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c   signature of the token for verification
+
+// JWT is used to authenticate and authorize users in an application                
+// It is a compact, URL-safe means of representing claims to be transferred between two parties
+// The claims in a JWT are encoded as a JSON object that is used as the payload of a JSON Web Signature
+//  (JWS) structure or as the plaintext of a JSON Web Encryption (JWE) structure, enabling the claims to be digitally 
+// signed or encrypted     
